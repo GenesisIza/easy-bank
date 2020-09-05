84e39fa7a6c11c49987617d4483cce9b5d3353f1
Schierke1 <36467193+Schierke0@users.noreply.github.com>
2020-09-04 23:17:14 -0600

    Initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+    return <nav>
+                <div className="nav-header flex-item">
+                <img src={process.env.PUBLIC_URL + '/images/logo.svg'} alt="fuck" />
+                </div>
+                <div className="flex-item nav-links ">
+                    <span>Home</span>
+                    <span>About</span>
+                    <span>Contact</span>
+                    <span>Blog</span>
+                    <span>Careers</span>
+                </div>
+                <div className="flex-item nav-button ">
+                  <button>Request Invite</button>
+                </div>
+                <div className="flex-item nav-button-movile">
+                  &#9776;
+                </div>
+            </nav>
+}
+
+export default Navbar;
