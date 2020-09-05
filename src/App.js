8b8d0c4f51a0679c4ff8d753f1878bc20d1adff8
Schierke1 <36467193+Schierke0@users.noreply.github.com>
2020-09-05 02:23:26 -0600

    .
    .

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header/Header';
+import Suggestions from './components/Suggestions/Suggestions';
 
 function App() {
   return (
     <main id="flex-container">
       <Navbar/>
       <Header/>
+      <Suggestions/>
     </main>
     
   );
---
+++ src/components/Suggestions/Suggestions.jsx
@@ -0,0 +1,50 @@
+import React from "react"
+import "./suggestions.css"
+
+const Suggestions = () =>(
+    <div className="flex-container-sug">
+       
+        <div className="flex-item-sug">
+            <img src={process.env.PUBLIC_URL + '/images/icon-online.svg'} alt=""/>
+            <h1>Online Banking</h1>
+            <p>
+                Our modern web and mobile applications allow you to keep track of your finances
+                wherever you are in the world.
+            </p>
+        </div>
+        <div className="flex-item-sug">
+            <img src={process.env.PUBLIC_URL + '/images/icon-budgeting.svg'} alt=""/>
+            <h1> Simple Budgeting</h1>
+            <p>
+                See exactly where your money goes each month.Receive notifications when you’re
+                close to hitting your limits.
+            </p>
+        </div>
+        <div className="flex-item-sug">
+            <img src={process.env.PUBLIC_URL + '/images/icon-onboarding.svg'} alt=""/>
+            <h1>Fast Onboarding</h1>
+            <p>
+                We don’t do branches.Open your account in minutes online and start taking control
+                of your finances right away.
+            </p>
+        </div>
+        <div className="flex-item-sug">
+            <img src={process.env.PUBLIC_URL + '/images/icon-api.svg'} alt=""/>
+            <h1>Open API</h1>
+            <p>
+                Manage your savings, investments, pension, and much more from one account.Tracking
+                your money has never been easier.
+            </p>
+        </div>
+    </div>
+)
+
+export default Suggestions
+
+
+
+
+
+
+
+
